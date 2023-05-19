17f7058ecd9144950269b16a909fed3d6660b509
bita333 <b.nkpoper33@gmail.com>
2023-05-19 08:17:51 +0430

    added model file and person class

---
+++ maktab-cw5/Model/person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maktab_cw5.Model
+{
+    public class person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NationalCode { get; set; }
+        public int Mobile { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+        public string Gender{ get; set; }
+        public string City { get; set; }
+        public string EmailAddress { get; set; }
+
+
+
+    }
+
+}
